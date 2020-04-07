cf0b3a93a60c59b1d96f105e3cfdeb4e83671412
brunoharlein <bruno@harlein.com>
2020-04-07 14:05:29 +0200

    end section 1 medium JS

--- JS_training/js/main.js
+++ JS_training/js/main.js
@@ -36,7 +36,7 @@ Attention aux espaces*/
 var nom = "Harlein";
 var prenom = "Bruno";
 
-console.log("Bonjour " + prenom +  " " + nom);
+console.log("Bonjour " + prenom + " " + nom);
 
 
 /*## 5\.Exercice 5 : Des caractères au nombre
@@ -70,16 +70,14 @@ var un = "";
 var deux = "test";
 
 if (un.length === 0) {
-  console.log (false);
-}
-else {
+  console.log(false);
+} else {
   console.log(true);
 }
 
 if (deux.length === 0) {
-  console.log (false);
-}
-else {
+  console.log(false);
+} else {
   console.log(true);
 }
 
@@ -90,12 +88,12 @@ Pour aller plus loin, demandez l'âge de la personne à côté de vous et affich
 
 // Get the current date and the birthDate by the user
 // Récupère la date actuelle et la date de naissance par l'utilisateur
-var currentDate =  prompt("En quelle année sommes-nous ?");
+var currentDate = prompt("En quelle année sommes-nous ?");
 var birthDate = prompt("Votre année de naissance ?");
 
 // Convert the strings into numbers
 // Convertit les chaînes en nombres
-currentDate =  Number(currentDate);
+currentDate = Number(currentDate);
 birthDate = Number(birthDate);
 
 // Count the age of the user and show it
@@ -105,7 +103,7 @@ alert("Vous avez " + age + " ans");
 
 // Get the age of the neighbour and convert it to number
 // Obtenir l'âge du voisin et le convertir en nombre
-var nextAge  = prompt("Quelle âge a la personne à côté de vous ?");
+var nextAge = prompt("Quelle âge a la personne à côté de vous ?");
 nextAge = Number(nextAge);
 
 // Count the sum of the two ages and show it
@@ -127,18 +125,18 @@ var prix3 = 20;
 var total = prix1 + prix2 + prix3;
 
 alert("Le total est de : " + total);
-console.log(total*0.8);
-alert("Avec la réduction le total est de : " + total*0.8);
+console.log(total * 0.8);
+alert("Avec la réduction le total est de : " + total * 0.8);
 
 
 /*## 4\.Exercice 4 : une calculatrice JavaScript
 Vous allez créer une mini calculatrice qui permet d'additionner des nombres.
  Quand le visiteur arrive sur la page vous lui demandez les nombres à additionner, puis vous affichez le résultat à l'écran.*/
 
- var firstNumber = parseInt(prompt("Quel est le premier nombre ?"));
- var secondNumber = parseInt(prompt("Quel est le deuxième nombre ?"));
- alert("la somme est de : " + (firstNumber + secondNumber));
- console.log(firstNumber + secondNumber);
+var firstNumber = parseInt(prompt("Quel est le premier nombre ?"));
+var secondNumber = parseInt(prompt("Quel est le deuxième nombre ?"));
+alert("la somme est de : " + (firstNumber + secondNumber));
+console.log(firstNumber + secondNumber);
 
 
 /*## 5\.Exercice 5 : travailler avec les propriétés
@@ -159,8 +157,8 @@ console.log("Voici la première et la dernière lettre de votre prénom" + fnLet
 
 console.log("Voici les deux mis ensemble " + lnLetters + fnLetters);
 
-var age =  parseInt(prompt("Quel est votre âge ?"));
-var age = Math.round(age/33);
+var age = parseInt(prompt("Quel est votre âge ?"));
+var age = Math.round(age / 33);
 
 console.log("Divisé par 33 cela donner " + age);
 
@@ -175,16 +173,13 @@ var thirdNumber = 65;
 var fourthNumber = 64;
 
 function bigNumber(a, b, c, d) {
-  if(a > b && a > c && a > d) {
+  if (a > b && a > c && a > d) {
     console.log(a);
-  }
-  else if(b > a && b > c && b > d) {
+  } else if (b > a && b > c && b > d) {
     console.log(b)
-  }
-  else if(c > a && c > b && c > d) {
+  } else if (c > a && c > b && c > d) {
     console.log(c);
-  }
-  else {
+  } else {
     console.log(d);
   }
 }
@@ -202,24 +197,22 @@ Sinon affichez un message d'erreur s'il n'a rien rentré
 Attention à l'ordre dans lequel vous effectuez vos tests !*/
 
 var userAge = Number(prompt("Quel âge avez-vous ?"));
-if(userAge !== 0) {
-  if(userAge <= 0) {
+if (userAge !== 0) {
+  if (userAge <= 0) {
     alert("Rentrez votre vrai âge !");
   }
   if (userAge >= 21) {
     alert("vous pouvez entrer !");
   }
-  if(userAge%2 === 0) {
+  if (userAge % 2 === 0) {
     alert("Votre âge est paire");
   }
-  if(Number.isInteger(Math.sqrt(userAge))) {
+  if (Number.isInteger(Math.sqrt(userAge))) {
     alert("Votre âge est un carré");
   }
-}
-else {
+} else {
   alert("Vous n'avez rien rentré !")
 }
-
 
 
 /*## 3\.Exercice 3 : Le nombre caché
@@ -229,19 +222,16 @@ Si l'utilisateur trouve le bon nombre vous lui annoncez qu'il a gagné. Sinon vo
 var hiddenNumber = 17;
 var userGuess = null;
 
-while(hiddenNumber !== userGuess) {
+while (hiddenNumber !== userGuess) {
   userGuess = Number(prompt("Quel est le nombre caché ?"));
-  if(userGuess > hiddenNumber) {
+  if (userGuess > hiddenNumber) {
     alert("Trop haut !");
-  }
-  else if (userGuess < hiddenNumber) {
+  } else if (userGuess < hiddenNumber) {
     alert("Trop bas");
-  }
-  else {
+  } else {
     alert("Trouvé !");
   }
 }
-
 
 
 /*## 4\.Exercice 4 : Des nombres en boucle
@@ -252,33 +242,30 @@ Exemple :
 3
 ...*/
 
-for(var i = 1; i <= 100; i++) {
+for (var i = 1; i <= 100; i++) {
   console.log(i);
 }
-
 
 
 /*## 5\.Exercice 5 : Des nombres en boucle bis
 Créez une boucle allant de 1 à 100, et n'affichez dans la console que les chiffres pairs.*/
 
-for(var i = 1; i <= 100; i++) {
-  if(i%2 === 0) {
+for (var i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
     console.log(i);
   }
 }
-
 
 
 /*## 6\.Exercice 6 : Remplir la piscine
 Ecrire une fonction en Javascript qui indique le temps de remplissage d'une piscine en fonction de sa longueur, sa largeur, sa profondeur, et du débit d'eau. Les longueurs sont en mètre (m), le débit en mètre cube par minute (m3/min) et le résultat attendu, le temps, en minute (min).*/
 
 function remplirPiscine(long, larg, prof, debit) {
-  var tempsRemplissage = (long*larg*prof)/debit;
+  var tempsRemplissage = (long * larg * prof) / debit;
   alert("Le temps de remplissage est de " + tempsRemplissage + " minutes")
 }
 
 remplirPiscine(13, 5, 3, 12);
-
 
 
 /*## 7\.Exercice 7 : Calcul de cercle
@@ -303,7 +290,6 @@ getPerimetre(rayon);
 getAire(rayon);
 
 
-
 /*## 8\.Exercice 8 : Une pyramide
 Ecrire un programme affichant dans la console le résultat suivant:
 
@@ -315,6 +301,27 @@ Ecrire un programme affichant dans la console le résultat suivant:
 *****
 ```*/
 
+var pyramide = "\n";
+var row = "";
+for (var i = 1; i < 6; i++) {
+  row += "*";
+  pyramide += row + "\n";
+}
+
+console.log(pyramide);
+
 
 /*## 9\.Exercice 9 : FIZZ BUZZ
 Ecrire un programme qui affiche les nombres de 1 à 100. Quand le nombre est un multiple de 3 afficher "FIZZ", quand le nombre est un multiple de 5 afficher "BUZZ" et quand le nombre est un multiple de 3 et 5 afficher "FIZZBUZZ"*/
+
+for (var i = 1; i <= 100; i++) {
+  if (i % 15 === 0) {
+    console.log("FIZZBUZZ");
+  } else if (i % 5 === 0) {
+    console.log("BUZZ");
+  } else if (i % 3 === 0) {
+    console.log("FIZZ");
+  } else {
+    console.log(i);
+  }
+}
