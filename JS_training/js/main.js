5e3d50ee59e176c9f4ba48d5ece79da231e58394
brunoharlein <bruno@harlein.com>
2020-04-07 13:39:29 +0200

    add medium exos

--- JS_training/js/main.js
+++ JS_training/js/main.js
@@ -135,6 +135,12 @@ alert("Avec la réduction le total est de : " + total*0.8);
 Vous allez créer une mini calculatrice qui permet d'additionner des nombres.
  Quand le visiteur arrive sur la page vous lui demandez les nombres à additionner, puis vous affichez le résultat à l'écran.*/
 
+ var firstNumber = parseInt(prompt("Quel est le premier nombre ?"));
+ var secondNumber = parseInt(prompt("Quel est le deuxième nombre ?"));
+ alert("la somme est de : " + (firstNumber + secondNumber));
+ console.log(firstNumber + secondNumber);
+
+
 /*## 5\.Exercice 5 : travailler avec les propriétés
 Demander son prénom au visiteur.
 Demander son nom au visiteur.
@@ -142,3 +148,78 @@ Afficher dans un message la lettre par laquelle son prénom commence et la lettr
 Faire de même pour son nom.
 Afficher dans un message la combinaison des lettres, par exemple pour Marc Gemini : "MCGI".
 Demander son âge au visiteur  et lui afficher le résultat de celui-ci divisé par 33. S'assurer que si ce nombre comporte une virgule alors il sera arrondi à l'entier supérieur ou inférieur.*/
+
+var lastName = prompt("votre nom ?").toUpperCase();
+var lnLetters = lastName[0] + lastName[lastName.length - 1];
+console.log("Voici la première et la dernière lettre de votre nom" + lnLetters);
+
+var firstName = prompt("Quel est votre prénom ?").toUpperCase();
+var fnLetters = firstName[0] + firstName[firstName.length - 1];
+console.log("Voici la première et la dernière lettre de votre prénom" + fnLetters);
+
+console.log("Voici les deux mis ensemble " + lnLetters + fnLetters);
+
+var age =  parseInt(prompt("Quel est votre âge ?"));
+var age = Math.round(age/33);
+
+console.log("Divisé par 33 cela donner " + age);
+
+
+/*## 1\.Exercice 1 : Le nombre le plus grand
+Déclarer 4 variables ayant chacune pour valeur un chiffre différent.
+Ecrire un programme renvoyant dans la console la valeur la plus élevée.*/
+
+
+/*## 2\.Exercice 2 : Conditon d'âge
+Ecrire un programme JavaScript qui demande son âge à l'utilisateur. Si l'utilisateur a rentré quelque chose et que :
+- l'age est négatif afficher un message qui demande d'entrer un âge réel
+- l'age est supérieur ou égal à 21 ans afficher un message autorisant l'accès
+- l'age est paire afficher un message indiquant à l'utilisateur que son âge est pair
+- l'age est un carré afficher un message indiquant à l'utilisateur que son âge est un nombre carré
+Sinon affichez un message d'erreur s'il n'a rien rentré
+Attention à l'ordre dans lequel vous effectuez vos tests !*/
+
+
+/*## 3\.Exercice 3 : Le nombre caché
+Stockez un nombre entier de votre choix dans une variable. Ecrivez ensuite un programme qui demande à l'utilisateur de deviner ce nombre.
+Si l'utilisateur trouve le bon nombre vous lui annoncez qu'il a gagné. Sinon vous lui dites si son nombre est plus grand ou plus petit que le nombre caché. Tant que l'utilisateur n'a pas trouvé le bon nombre vous lui reposez la question.*/
+
+
+/*## 4\.Exercice 4 : Des nombres en boucle
+Ecrire un programme qui affiche à l'écran les nombre de 1 à 100 en revenant à chaque fois à la ligne.
+Exemple :
+1
+2
+3
+...*/
+
+
+/*## 5\.Exercice 5 : Des nombres en boucle bis
+Créez une boucle allant de 1 à 100, et n'affichez dans la console que les chiffres pairs.*/
+
+
+/*## 6\.Exercice 6 : Remplir la piscine
+Ecrire une fonction en Javascript qui indique le temps de remplissage d'une piscine en fonction de sa longueur, sa largeur, sa profondeur, et du débit d'eau. Les longueurs sont en mètre (m), le débit en mètre cube par minute (m3/min) et le résultat attendu, le temps, en minute (min).*/
+
+
+/*## 7\.Exercice 7 : Calcul de cercle
+Ecrire 2 fonctions, l'une retournant l'aire d'un cercle, l'autre son périmètre.
+Le Rayon devra être entrer manuellement à l'aide d'un "prompt()".
+Les résultats des deux fonctions seront affichés dans la console.
+Pour aller plus loin, arrondissez les résultats à la deuxième décimale.*/
+
+
+/*## 8\.Exercice 8 : Une pyramide
+Ecrire un programme affichant dans la console le résultat suivant:
+
+```
+*
+**
+***
+****
+*****
+```*/
+
+
+/*## 9\.Exercice 9 : FIZZ BUZZ
+Ecrire un programme qui affiche les nombres de 1 à 100. Quand le nombre est un multiple de 3 afficher "FIZZ", quand le nombre est un multiple de 5 afficher "BUZZ" et quand le nombre est un multiple de 3 et 5 afficher "FIZZBUZZ"*/
